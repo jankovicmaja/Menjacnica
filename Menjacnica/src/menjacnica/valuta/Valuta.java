44baa2b04fde5717f001e34b71c8520fe6596601
Home <Home@192.168.40.232>
2018-03-20 22:19:00 +0100

    U obe klase(Valuta,Menjacnica) su dodate metode equals(), toString() i hashCode()

--- Menjacnica/src/menjacnica/valuta/Valuta.java
+++ Menjacnica/src/menjacnica/valuta/Valuta.java
@@ -45,4 +45,58 @@ public class Valuta {
 		public void setSrednjiKurs(double srednjiKurs) {
 			this.srednjiKurs = srednjiKurs;
 		}
+		@Override
+		public String toString() {
+			return "Valuta [ime=" + ime + ", skracenoIme=" + skracenoIme + ", datum=" + datum + ", prodajniKurs="
+					+ prodajniKurs + ", kupovniKurs=" + kupovniKurs + ", srednjiKurs=" + srednjiKurs + "]";
+		}
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+			result = prime * result + ((ime == null) ? 0 : ime.hashCode());
+			long temp;
+			temp = Double.doubleToLongBits(kupovniKurs);
+			result = prime * result + (int) (temp ^ (temp >>> 32));
+			temp = Double.doubleToLongBits(prodajniKurs);
+			result = prime * result + (int) (temp ^ (temp >>> 32));
+			result = prime * result + ((skracenoIme == null) ? 0 : skracenoIme.hashCode());
+			temp = Double.doubleToLongBits(srednjiKurs);
+			result = prime * result + (int) (temp ^ (temp >>> 32));
+			return result;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Valuta other = (Valuta) obj;
+			if (datum == null) {
+				if (other.datum != null)
+					return false;
+			} else if (!datum.equals(other.datum))
+				return false;
+			if (ime == null) {
+				if (other.ime != null)
+					return false;
+			} else if (!ime.equals(other.ime))
+				return false;
+			if (Double.doubleToLongBits(kupovniKurs) != Double.doubleToLongBits(other.kupovniKurs))
+				return false;
+			if (Double.doubleToLongBits(prodajniKurs) != Double.doubleToLongBits(other.prodajniKurs))
+				return false;
+			if (skracenoIme == null) {
+				if (other.skracenoIme != null)
+					return false;
+			} else if (!skracenoIme.equals(other.skracenoIme))
+				return false;
+			if (Double.doubleToLongBits(srednjiKurs) != Double.doubleToLongBits(other.srednjiKurs))
+				return false;
+			return true;
+		}
+		
 }
